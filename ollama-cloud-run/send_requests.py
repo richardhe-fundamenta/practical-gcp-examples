8296f287c809365c8bca7e8197ed10aa3dcad5bf
Richard He <shenghua.he@rocketech.co.uk>
2025-01-21 14:16:34 +0000

    Added prompt generator

--- ollama-cloud-run/send_requests.py
+++ ollama-cloud-run/send_requests.py
@@ -1,12 +1,16 @@
 import aiohttp
 import asyncio
+import de_prompt_gen
+
+TOTAL_CONCURRENT_REQUESTS = 15
+ALL_PROMPTS = [_ for _ in de_prompt_gen.generate(TOTAL_CONCURRENT_REQUESTS)]
 
 
-async def send_request(session):
+async def send_request(session, index):
     url = "http://localhost:9090/api/generate"
     payload = {
         "model": "deepseek-r1:7b",
-        "prompt": "Why is the sky blue?"
+        "prompt": ALL_PROMPTS[index]
     }
     try:
         async with session.post(url, json=payload) as response:
@@ -20,7 +24,7 @@ async def send_request(session):
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        tasks = [send_request(session) for _ in range(10)]
+        tasks = [send_request(session, index) for index in range(TOTAL_CONCURRENT_REQUESTS)]
         results = await asyncio.gather(*tasks)
         for result in results:
             print(result)
